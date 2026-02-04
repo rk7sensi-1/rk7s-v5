7bb65db07d062a1366faecdf53f40f3531b2c6ec
rk7sensi-1 <jg2814313@gmail.com>
2026-02-04 08:54:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,45 @@
 const CACHE_NAME = 'spotify-pwa-v1';
 const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'icons/icon-192.png',
-  'icons/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 // Instalação
 self.addEventListener('install', event => {
+  console.log('[SW] Instalando...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+      .then(cache => {
+        console.log('[SW] Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log('[SW] Arquivos cacheados');
+        return self.skipWaiting();
+      })
+      .catch(err => console.error('[SW] Erro ao cachear:', err))
+  );
+});
+
+// Ativar
+self.addEventListener('activate', event => {
+  console.log('[SW] Ativando...');
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              console.log('[SW] Deletando cache antigo:', cacheName);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
 
@@ -19,18 +47,17 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// Ativar
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    }).then(() => self.clients.claim())
+      .then(response => {
+        if (response) {
+          console.log('[SW] Servindo do cache:', event.request.url);
+          return response;
+        }
+        console.log('[SW] Buscando da rede:', event.request.url);
+        return fetch(event.request);
+      })
+      .catch(err => {
+        console.error('[SW] Erro no fetch:', err);
+        return caches.match('./index.html');
+      })
   );
 });
